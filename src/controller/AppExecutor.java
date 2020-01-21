2b5a87f1c9d2e865eb269e2dc3a5d24d4348dac3
sviatoslav <svyatoslav.grinik@gmail.com>
2020-01-21 15:55:37 +0100

    task2/lfu: removed empty lines

--- src/controller/AppExecutor.java
+++ src/controller/AppExecutor.java
@@ -5,14 +5,12 @@ import service.News;
 
 public class AppExecutor {
     private News news = new News();
+    private LFUCache cache = new LFUCache(5);
 
     public void runApp() {
         news.putToNewsList();
 
-        LFUCache cache = new LFUCache(5);
-
         for (long i = 1L; i <= 10L; i++) {
-
             cache.put(i, news.getNews("News #" + i));
             cache.put(2L, news.getNews("News #1"));
             cache.put(3L, news.getNews("News #2"));
